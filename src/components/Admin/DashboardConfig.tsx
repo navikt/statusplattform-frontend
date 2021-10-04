f8c920e1b57dd9e239dfc398cba8b9e2097c661e
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2021-10-04 15:57:08 +0200

    Mer støtte til dashbord-tab

--- src/components/Admin/DashboardConfig.tsx
+++ src/components/Admin/DashboardConfig.tsx
@@ -1,15 +1,114 @@
+import { Hovedknapp } from 'nav-frontend-knapper'
+import { Input } from 'nav-frontend-skjema'
+import NavFrontendSpinner from 'nav-frontend-spinner'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 import styled from 'styled-components'
+import { Dashboard } from 'types/navServices'
+import { fetchDashboards } from 'utils/fetchDashboards'
+import { postDashboards } from 'utils/postDashboard'
+import { useLoader } from 'utils/useLoader'
 
 const DashboardConfigContainer = styled.div`
+
 `
 
+const SpinnerCentered = styled.div`
+    position: absolute;
+    top: 40%;
+`
+
+
 const DashboardConfig = () => {
+    const { data: dashboards, isLoading, reload } = useLoader(fetchDashboards,[]);
+
+
+    if (isLoading) {
+        return (
+            <LoaderSpinner/>
+        ) 
+    }
 
     return (
         <DashboardConfigContainer>
-            testasdasdasd
+            <DashboardTable dashboards={dashboards} />
+            <AddNewDashboard />
         </DashboardConfigContainer>
     )
 }
-            
+
+
+/* Helpers below */
+
+const LoaderSpinner = () => {
+    return (
+        <SpinnerCentered>
+            <NavFrontendSpinner type="XXL" />
+        </SpinnerCentered>
+    )
+}
+
+const DashboardTable: React.FC<{dashboards: Dashboard[]}> = ({dashboards}) => {
+    return (
+        <table className="tabell tabell--stripet">
+            <thead>
+                <tr>
+                    <th>Navn på Dashbord</th>
+                </tr>
+            </thead>
+            <tbody>
+                {dashboards.map((dashboard) => {
+                    return (
+                        <tr key={dashboard.name}>
+                            <td>
+                                {dashboard.name}
+                            </td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
+}
+
+
+const AddNewDashboard = () => {
+    const [newDashboard, updateNewDashboard] = useState<Dashboard>({
+        name: "",
+        areaIds: [""]
+    })
+
+
+    const handleChangeDashboardName = (event) => {
+        const changedDashboard = {
+            name: event.target.value,
+            areaIds: [""]
+        }
+        updateNewDashboard(changedDashboard)
+    }
+    
+
+    const handlePostNewDashboard = () => {
+        // postDashboards(newDashboard)
+        toast.info("Kan ikke laste opp enda")
+    }
+
+    const { name } = newDashboard
+
+    return (
+        <table className="tabell tabell--stripet add-new-dashboard">
+            <tbody>
+                <tr>
+                    <td colSpan={2}>
+                        <Input type="text" required value={name} onChange={(event) => handleChangeDashboardName(event)} placeholder="Navn*" />
+                    </td>
+                    <td>
+                        <Hovedknapp disabled={!name} value="Legg til dashbord" onClick={() => handlePostNewDashboard()}>Legg til</Hovedknapp>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
 export default DashboardConfig
---
+++ src/utils/postDashboard.ts
@@ -0,0 +1,46 @@
+import { Dashboard } from "types/navServices";
+
+export class ResponseError extends Error {
+    public constructor (message: string, public response: Response) {
+        super(message)
+    }
+}
+
+export const postDashboards = async (dashboard: Dashboard): Promise<Object[]> =>{
+    let response;
+    let endPath = "/rest/Dashboards/"
+
+    if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+        response = await fetch("http://localhost:3001" + endPath,
+        {
+            method: "POST",
+            body: JSON.stringify({
+                name: dashboard.name
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            mode: 'cors', // no-cors, *cors, same-origin,
+            credentials: 'same-origin', // include, *same-origin, omit
+
+        });
+    }
+    else {
+        response = await fetch("https://digitalstatus.ekstern.dev.nav.no" + endPath,
+        {
+            method: "POST",
+            body: JSON.stringify({
+                name: dashboard.name
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            mode: 'cors', // no-cors, *cors, same-origin
+        });
+    }
+
+    if (response.ok) {
+        return response.json()
+    }
+    throw new ResponseError("Failed to post to server", response)
+}
