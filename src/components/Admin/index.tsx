c20e97db805de5d8cecfaad5c8ffb3d07c1dba6d
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2021-10-27 15:44:42 +0200

    Fjernet gammel kode

--- src/components/Admin/index.tsx
+++ src/components/Admin/index.tsx
@@ -1,10 +1,8 @@
 import 'react-dropdown/style.css';
 import styled from 'styled-components'
-import { useState } from "react";
 
 import AreaTable from './AreaTable';
 import TjenesteTable from './TjenesteTable';
-import { Select } from 'nav-frontend-skjema';
 import DashboardConfig from './DashboardConfig';
 import { adminMenu, useFindCurrentTab } from './MenuSelector';
 
@@ -12,41 +10,11 @@ import { adminMenu, useFindCurrentTab } from './MenuSelector';
 
 
 const AdminDashboardContainer = styled.div`
-    padding: 0;
     width: 100%;
+    padding: 0 0 3rem 0;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
-    th {
-        span {
-            display: flex;
-            justify-content: center;
-        }
-    }
-    tr {
-        td:nth-child(2) {
-            span {
-                text-align: left;
-                display: block;
-            }
-        }
-        td:nth-child(3) {
-            span {
-                text-align: left;
-                display: block;
-            }
-        }
-    }
-	td {
-		height: 75px;
-        span {
-            display: flex;
-            justify-content: center;
-        }
-	}
-    @media(min-width: 500px){
-    	padding: 0 0 3rem 0;
-    }
 `;
 
 
@@ -63,16 +31,6 @@ const AdminConfigsContainer = styled.div`
     }
 `;
 
-const AreaTableContainer = styled.div`
-    width: 100%;
-    /* overflow-x: auto; */
-`
-
-const CustomSelect = styled(Select)`
-    min-width: 100px;
-    max-width: 200px;
-`
-
 export interface Props {
     selectedMenu?: string
 }
@@ -85,9 +43,7 @@ const AdminDashboard = () => {
             <AdminConfigsContainer>
                 <h2>{selectedMenu}</h2>
                 {selectedMenu === "Områder" && 
-                    <AreaTableContainer>
-                        <AreaTable />
-                    </AreaTableContainer>
+                    <AreaTable />
                 }
                 {selectedMenu === "Tjenester" && 
                     <TjenesteTable />
