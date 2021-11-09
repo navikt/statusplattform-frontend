d0d915d336491fbc07c78b6ebc074e680214ad07
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2021-11-09 09:48:03 +0100

    Ny testing av meta-tags

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -40,19 +40,22 @@ const MainContent = props => {
                 <meta name="title" content="Navstatus" />
                 <meta name="description" content="" />
 
+
                 {/* <!-- Open Graph / Facebook --> */}
+                <meta property="og:site_name" content="Status Nav digitale tjenester" />
+                <meta property="og:title" content="Status Nav digitale tjenester" />
+                <meta property="og:description" content="Status Nav digitale tjenester er en oversiktsside for Navs ulike tjenester til borgere, arbeidsgivere og samarbeidspartnere." />
+                <meta property="og:image" content="https://www.nav.no/dekoratoren/media/nav-logo-red.svg" />
+                <meta property="og:url" content="https://portal.labs.nais.io/Dashboard/Privatperson" />
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://portal.labs.nais.io/" />
-                <meta property="og:title" content="Navstatus" />
-                <meta property="og:description" content="" />
-                <meta property="og:image" content="" />
+
 
                 {/* <!-- Twitter --> */}
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://portal.labs.nais.io/" />
+                <meta property="twitter:url" content="https://portal.labs.nais.io/Dashboard/Privatperson" />
                 <meta property="twitter:title" content="Navstatus" />
-                <meta property="twitter:description" content="" />
-                <meta property="twitter:image" content="" />
+                <meta property="twitter:description" content="Status Nav digitale tjenester er en oversiktsside for Navs ulike tjenester til borgere, arbeidsgivere og samarbeidspartnere." />
+                <meta property="twitter:image" content="https://www.nav.no/dekoratoren/media/nav-logo-red.svg" />
             </Head>
             <Navbar/>
             <Header/>
