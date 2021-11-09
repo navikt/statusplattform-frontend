787a50611dc23ca2dfc700de322e3ce997a29b35
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2021-11-09 10:07:35 +0100

    Siste test av meta tags

--- src/_document.js
+++ src/_document.js
@@ -25,27 +25,6 @@ export default class MyDocument extends Document {
         <Head>
           <title>Status digitale tjenester</title>
           {/* Step 5: Output the styles in the head  */}
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <meta name="title" content="Navstatus" />
-          <meta name="description" content="" />
-
-
-          {/* <!-- Open Graph / Facebook --> */}
-          <meta property="og:site_name" content="Status Nav digitale tjenester" />
-          <meta property="og:title" content="Status Nav digitale tjenester" />
-          <meta property="og:description" content="Status Nav digitale tjenester er en oversiktsside for Navs ulike tjenester til borgere, arbeidsgivere og samarbeidspartnere." />
-          <meta property="og:image" content="https://www.nav.no/dekoratoren/media/nav-logo-red.svg" />
-          <meta property="og:url" content="https://portal.labs.nais.io/Dashboard/Privatperson" />
-          <meta property="og:type" content="website" />
-
-
-          {/* <!-- Twitter --> */}
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://portal.labs.nais.io/Dashboard/Privatperson" />
-          <meta property="twitter:title" content="Navstatus" />
-          <meta property="twitter:description" content="Status Nav digitale tjenester er en oversiktsside for Navs ulike tjenester til borgere, arbeidsgivere og samarbeidspartnere." />
-          <meta property="twitter:image" content="https://www.nav.no/dekoratoren/media/nav-logo-red.svg" />
           {this.props.styleTags}
         </Head>
         <body>
