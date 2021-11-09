092f56338bc28ba80f5e2e0b993efc1bbb06c634
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2021-11-09 12:29:36 +0100

    Meta-test for lasting

--- src/components/CustomNavSpinner/index.tsx
+++ src/components/CustomNavSpinner/index.tsx
@@ -14,6 +14,33 @@ const SpinnerCentered = styled.div`
 const CustomNavSpinner = () => {
     return (
         <SpinnerCentered>
+            <Head>
+                <title>Lasting av navstatus</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="title" content="Navstatus" />
+                <meta name="description" content="Status Nav digitale tjenester er en oversiktsside for Navs ulike tjenester til borgere, arbeidsgivere og samarbeidspartnere." />
+                <meta property="image" content="https://www.nav.no/dekoratoren/media/nav-logo-red.svg" />
+                <meta property="url" content="https://portal.labs.nais.io/Dashboard/Privatperson" />
+                <meta property="type" content="website" />
+
+
+                {/* <!-- Open Graph / Facebook --> */}
+                <meta property="og:site_name" content="Status Nav digitale tjenester" />
+                <meta property="og:title" content="Status Nav digitale tjenester" />
+                <meta property="og:description" content="Status Nav digitale tjenester er en oversiktsside for Navs ulike tjenester til borgere, arbeidsgivere og samarbeidspartnere." />
+                <meta property="og:image" content="https://www.nav.no/dekoratoren/media/nav-logo-red.svg" />
+                <meta property="og:url" content="https://portal.labs.nais.io/Dashboard/Privatperson" />
+                <meta property="og:type" content="website" />
+
+
+                {/* <!-- Twitter --> */}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://portal.labs.nais.io/Dashboard/Privatperson" />
+                <meta property="twitter:title" content="Navstatus" />
+                <meta property="twitter:description" content="Status Nav digitale tjenester er en oversiktsside for Navs ulike tjenester til borgere, arbeidsgivere og samarbeidspartnere." />
+                <meta property="twitter:image" content="https://www.nav.no/dekoratoren/media/nav-logo-red.svg" />
+            </Head>
             <NavFrontendSpinner type="XXL"/>
         </SpinnerCentered>
     )
