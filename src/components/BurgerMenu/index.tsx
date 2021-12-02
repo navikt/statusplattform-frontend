740bbbec311753dd83a186a8f2225f47381add74
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2021-12-02 12:28:48 +0100

    Bug i commit gjorde at logout hadde stor L

--- src/components/BurgerMenu/index.tsx
+++ src/components/BurgerMenu/index.tsx
@@ -174,8 +174,9 @@ const PopoverContent = () => {
                                         Adminside
                                     </Lenke>
                                 </li>
+
                                 <li>
-                                    <Lenke href="/oauth2/logout">
+                                    < Lenke href="/oauth2/logout">
                                         <Login className="popover-link-ikon" />
                                         Logg ut
                                     </Lenke>
