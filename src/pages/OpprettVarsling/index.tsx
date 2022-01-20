d7052a6ee4fe5cd36f410129bebb721ff39f3cdd
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2022-01-20 15:14:57 +0100

    Path fix

--- src/pages/OpprettVarsling/index.tsx
+++ src/pages/OpprettVarsling/index.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect } from 'react'
 
 import Layout from '../../components/Layout'
 import CreateNotifications from '../../components/SetupNotifications'
-import { TitleContext } from 'src/components/ContextProviders/TitleContext'
+import { TitleContext } from '../../components/ContextProviders/TitleContext'
 
 const Varsling = () => {
     const { changeTitle } = useContext(TitleContext)
