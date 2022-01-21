63940b1479812c380b204349151ff69380bb03fb
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2022-01-21 15:27:58 +0100

    Notifications-page almost done

--- src/components/SetupNotifications/index.tsx
+++ src/components/SetupNotifications/index.tsx
@@ -1,13 +1,16 @@
 import styled from "styled-components"
+import Link from 'next/link'
 import { useState } from "react"
 
-import { BodyShort, Heading, Radio, RadioGroup, TextField } from "@navikt/ds-react"
+import { BodyShort, Button, Checkbox, Heading, Radio, RadioGroup, TextField } from "@navikt/ds-react"
+import { useRouter } from "next/router"
+import { toast } from "react-toastify"
+import { Refresh } from "@navikt/ds-icons"
 
 
 const NotificationsContainer = styled.div`
     display: flex;
     flex-direction: column;
-
     justify-self: center;
 
     .navds-radio-buttons {
@@ -29,17 +32,25 @@ const NotificationsContainer = styled.div`
         }
     }
 
-    @media (min-width: 600px) {
-        max-width: 600px;
+    @media (min-width: 430px) {
+        width: 430px;
     }
 `
 
 const CreateNotifications = () => {
     const [chosenOTP, changeChosenOTP] = useState<string>("E-post")
     const [phone, updatePhone] = useState<string>()
-    const [email, updateEmail] = useState<string>()
+    const [epost, updateEmail] = useState<string>()
 
     const [emailSelected, changeEmailSelected] = useState<boolean>(true)
+    const [acceptsTerms, updateAcceptsTerms] = useState<string>("")
+
+    const [clickedSendOtp, changeClickedSendOtp] = useState<boolean>(false)
+
+    const router = useRouter()
+
+
+
 
     const handleChangeOTP = (event) => {
         changeChosenOTP(event)
@@ -60,8 +71,23 @@ const CreateNotifications = () => {
         updatePhone(event.target.value)
     }
 
-    
-    console.log(email + " " + phone)
+    const handleUpdateAcceptsTerms = (event) => {
+        console.log(event)
+    }
+
+
+
+
+
+    if(clickedSendOtp) {
+        return <Otp phoneNumber={phone} changeClickedSendOtp={(state: boolean) => changeClickedSendOtp(state)}/>
+    }
+
+
+
+
+
+
 
     return (
         <NotificationsContainer>
@@ -70,26 +96,75 @@ const CreateNotifications = () => {
             <BodyShort>For å opprette en profil, trenger vi å sende deg et engangspassord</BodyShort>
 
             <form>
-                <RadioGroup legend="Hvordan vil du ha passordet tilsendt?" defaultValue="E-post" onChange={(event) => handleChangeOTP(event)}>
+                <RadioGroup legend="Hvordan vil du ha passordet tilsendt?" defaultValue={(chosenOTP) ? chosenOTP : "E-post"} onChange={(event) => handleChangeOTP(event)}>
                     <Radio value="E-post">E-post</Radio>
                     <Radio value="Telefon">Telefon</Radio>
                 </RadioGroup>
 
-                {chosenOTP == "E-post" ?
-                        <div className="otp-input">
-                            <BodyShort>Fyll inn din epost i feltet nedenfor, så sender vi deg engangspassordet på mail</BodyShort>
-                            <TextField label="E-post" value={phone} onChange={event => handleUpdatePhone(event)} />
-                        </div>
-                    :
-                        <div className="otp-input">
-                            <BodyShort>Fyll inn ditt telefonnummer i feltet under, så sender vi deg engangspassordet på SMS</BodyShort>
-                            <TextField label="Sms" value={email} onChange={event => handleUpdatePhone(event)} />
-                        </div>
-                }
+                <div className="otp-input">
+                    <BodyShort>Fyll inn din epost i feltet nedenfor, så sender vi deg engangspassordet på {emailSelected ? "Epost" : "SMS"}</BodyShort>
+                    <TextField label="E-post" value={phone} onChange={event => handleUpdatePhone(event)} />
+                </div>
+
+                <Checkbox value={acceptsTerms} onChange={event => handleUpdateAcceptsTerms(event)} >Jeg godtar <Link href="#">personvernerklæring og vilkår for bruk</Link></Checkbox>
+                
+                <ButtonContainer>
+                    <Button onClick={() => router.push("/")}>Avbryt</Button>
+
+                    <Button onClick={() => changeClickedSendOtp(true)}>Send passord på {emailSelected ? "Epost" : "SMS"}</Button>
+                </ButtonContainer>
             </form>
 
         </NotificationsContainer>
     )
 }
+
+
+
+
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    
+    @media(min-width: 300px) {
+        flex-direction: row;
+    }
+`
+
+
+const OtpContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    > * {
+        margin-bottom: 20px;
+    }
+`
+
+
+
+
+const Otp: React.FC<{phoneNumber: string, changeClickedSendOtp: (state) => void }> = ({phoneNumber, changeClickedSendOtp}) => {
+    return (
+        <OtpContainer>
+            <Heading level="2" size="medium">Fyll inn passordet fra SMS</Heading>
+            
+            <BodyShort>Fyll inn engangspassordet du mottok på SMS.</BodyShort>
+
+            <BodyShort>Mottok du ikke passordet? <Refresh /> <span onClick={() => toast.info("Nytt passord sent")}>Send passordet på nytt</span></BodyShort>
+
+            <TextField label="Engangspassord fra SMS" />
+
+            <ButtonContainer>
+                <Button onClick={() => changeClickedSendOtp(false)}>Avbryt</Button>
+                <Button>Opprett profil</Button>
+            </ButtonContainer>
+        </OtpContainer>
+    )
+}
+
+
+
 
 export default CreateNotifications
