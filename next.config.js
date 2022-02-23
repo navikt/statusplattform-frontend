5f5409559ab50399106cf0513636a0bc0bc2fffa
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2022-02-23 15:51:18 +0100

    Additional cleanup and removal of unused files/folders

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withLess = require("@zeit/next-less");
 const packageJson = require("./package.json");
 const withTM = require("next-transpile-modules")(["@navikt/ds-icons"]);
 
+
 module.exports = withPlugins(
   [
     withTranspileModules(
@@ -17,34 +18,37 @@ module.exports = withPlugins(
     withTM,
   ],
 
+
 {
   i18n: {
     locales: ['no'],
     defaultLocale: 'no',
   },
+
   async rewrites() {
     // sett opp miljøvar
     return process.env.NODE_ENV !== 'production'
-    ? [
-        {
-          source: '/oauth2/:path*',
-          destination: `http://localhost:3005/oauth2/:path*`
-        },
-        {
-          source: '/rest/:path*',
-          destination: `http://localhost:3005/rest/:path*`
-        },
-      ]
-    : [
-        {
-          source: '/oauth2/:path*',
-          destination: `https://digitalstatus.ekstern.dev.nav.no/oauth2/:path*`
-        },
-        {
-          source: '/rest/:path*',
-          destination: `https://digitalstatus.ekstern.dev.nav.no/rest/:path*`
-        }
-      ];
-  }
-}
-);
+        ?
+            [
+                {
+                    source: '/oauth2/:path*',
+                    destination: `http://localhost:3005/oauth2/:path*`
+                },
+                {
+                    source: '/rest/:path*',
+                    destination: `http://localhost:3005/rest/:path*`
+                },
+            ]
+        :
+            [
+                {
+                    source: '/oauth2/:path*',
+                    destination: `https://digitalstatus.ekstern.dev.nav.no/oauth2/:path*`
+                },
+                {
+                    source: '/rest/:path*',
+                    destination: `https://digitalstatus.ekstern.dev.nav.no/rest/:path*`
+                }
+            ];
+    }
+});
