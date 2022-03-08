13b9107a8fa38c1114a9f42fc213a773555eb628
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2022-03-08 10:37:37 +0100

    Updated footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,96 +1,64 @@
 import styled from 'styled-components'
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import Image from 'next/image'
 
 import TrafficLights from '../../components/TrafficLights';
+import { BodyShort } from '@navikt/ds-react';
 
 const FooterContainer = styled.footer`
     width: 100%;
     margin-top: auto; /*Footer always at bottom (if min.height of container is 100vh)*/
+    padding: 0 1.5rem;
+    
     background-color: white;
     border-top: 1px solid #eaeaea;
 
-    display: flex;
-    justify-content: center;
-`
-
-const FooterContent = styled.div`
-    width: 100%;
-    max-width: 1200px;
-    padding: 1.75rem 1rem;
     display: flex;
     flex-direction: column;
 
     img {
         width: 63px;
+        margin-right: 1.2rem;
 
         :hover {
             transform: scale(1.05)
         }
     }
 
-    a {
-        color: var(--navds-global-color-blue-500);
-        text-decoration: underline;
-        :hover {
-            text-decoration: none;
-        }
-    }
-
-    p {
-        margin: 0;
-    }
-
-    @media (min-width: 700px){ 
+    span {
+        display: flex;
         flex-direction: row;
-    }
-`;
+        align-items: center;
 
-const Row = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-
-    &.top {
-        /* align-items: center; */
-        /* position: relative; */
-        width: auto;
+        min-height: 80px;
     }
 
-    &.bottom {
-        width: 100%;
-        padding: 0.5rem 10px;
-        margin: 0;
-        border-right: none;
-        justify-content: center;
-        flex-direction: column;
-    }
-
-    > ul {
+    ul {
         padding: 0;
         list-style: none;
-        width: 100%;
+        
         display: flex;
         flex-direction: column;
+
         li {
             padding: 0.625rem 0;
         }
     }
-    
-    @media (min-width: 700px) {
+
+    a {
+        color: black;
+    }
+
+    @media (min-width: 600px) {
+        padding: 0 50px;
+
         flex-direction: row;
+        justify-content: space-between;
 
-        > ul {
-            padding: 0;
-            margin: 0;
-            display: flex;
-            flex-flow: row wrap;
-
-            li {
-                margin: 0.375rem 2rem 0 0;
-                padding-top: 0;
-            }
+        ul {
+            flex-direction: row;
+            justify-content: flex-end;
+            gap: 1.5rem;
         }
     }
 `
@@ -113,31 +81,24 @@ const Footer = () => {
 
     return (
         <FooterContainer>
-            <FooterContent>
-                <Row className="top">
-                    <span>
-                        <a href="https://www.nav.no/no/person#" aria-label="Lenke til nav.no">
-                            <img src="/assets/nav-logo/png/black.png" alt="Til forsiden" aria-hidden="true" />
-                        </a>
-                    </span>
-                </Row>
+            <span>
+                <a href="https://www.nav.no/no/person#" aria-label="Lenke til nav.no">
+                    <img src="/assets/nav-logo/png/black.png" alt="Til forsiden" aria-hidden="true" />
+                </a>
+                <BodyShort>Arbeids- og velferdsetaten</BodyShort>
+            </span>
 
-                <Row className="bottom">
-                    <p>Arbeids- og velferdsetaten</p>
-                    <ul>
-                        <li><a href="https://www.nav.no/no/nav-og-samfunn/om-nav/personvern-i-arbeids-og-velferdsetaten">Personvern og informasjonskapsler</a></li>
-                        <li><a href="https://www.nav.no/no/nav-og-samfunn/kontakt-nav/teknisk-brukerstotte/nyttig-a-vite/tilgjengelighet">Tilgjengelighet</a></li>
-                        <li><a href="https://www.nav.no/no/person#">Del skjerm med veileder</a></li>
-                    </ul>
-                </Row>
-                
-                {/* <Separator style={{display: router.pathname.includes("Dashboard") ? "block" : "none"}} />
-                {router.pathname.includes("Dashboard") &&
-                    <div>
-                        <TrafficLights isInternal={isInternal}/>
-                    </div>
-                } */}
-            </FooterContent>
+                <ul>
+                    <li><a href="https://www.nav.no/no/nav-og-samfunn/om-nav/personvern-i-arbeids-og-velferdsetaten">Personvern og informasjonskapsler</a></li>
+                    <li><a href="https://www.nav.no/no/nav-og-samfunn/kontakt-nav/teknisk-brukerstotte/nyttig-a-vite/tilgjengelighet">Tilgjengelighet</a></li>
+                </ul>
+        
+            {/* <Separator style={{display: router.pathname.includes("Dashboard") ? "block" : "none"}} />
+            {router.pathname.includes("Dashboard") &&
+                <div>
+                    <TrafficLights isInternal={isInternal}/>
+                </div>
+            } */}
         </FooterContainer>
     )
 }
