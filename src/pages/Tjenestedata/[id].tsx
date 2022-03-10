d653ad9f665955d3a0c3c8c1ecabd7a04f0f7a6f
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2022-03-10 12:33:49 +0100

    Removed additional old code

--- src/pages/Tjenestedata/[id].tsx
+++ src/pages/Tjenestedata/[id].tsx
@@ -3,8 +3,6 @@ import Head from 'next/head'
 import Layout from '../../components/Layout'
 
 import CustomNavSpinner from '../../components/CustomNavSpinner'
-import { useLoader } from '../../utils/useLoader'
-import { useRenderComponentOnQuery } from '../../utils/routerHelpers'
 import TjenestedataContent from './TjenestedataComponent'
 import { fetchServiceFromId } from '../../utils/servicesAPI'
 import { useRouter } from 'next/router'
@@ -19,7 +17,6 @@ import { UserData } from '../../types/userData'
 
 
 const TjenestedataContainer = () => {
-    // const { data: service, isLoading, reload } = useLoader(() => fetchServiceFromId(idOfService),[]);
     const [isLoading, setIsLoading] = useState(false)
     const [service, setService] = useState<Service>()
     const router = useRouter()
