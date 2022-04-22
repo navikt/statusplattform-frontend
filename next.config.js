88c508659fe826064a39d1e112708f832a1e6185
KnasKnas <christian.rudfoss@gmail.com>
2022-04-22 13:39:44 +0200

    testing change of basePath

--- next.config.js
+++ next.config.js
@@ -1,54 +1,53 @@
-const withPlugins = require("next-compose-plugins");
-const withTranspileModules = require("next-transpile-modules");
-const withCss = require("@zeit/next-css");
-const withLess = require("@zeit/next-less");
-const packageJson = require("./package.json");
-const withTM = require("next-transpile-modules")(["@navikt/ds-icons"]);
-
+const withPlugins = require("next-compose-plugins")
+const withTranspileModules = require("next-transpile-modules")
+const withCss = require("@zeit/next-css")
+const withLess = require("@zeit/next-less")
+const packageJson = require("./package.json")
+const withTM = require("next-transpile-modules")(["@navikt/ds-icons"])
 
 module.exports = withPlugins(
-  [
-    withTranspileModules(
-      Object.keys(packageJson.dependencies).filter((key) =>
-        key.startsWith("nav-frontend-")
-      )
-    ),
-    withCss,
-    withLess,
-    withTM,
-  ],
+	[
+		withTranspileModules(
+			Object.keys(packageJson.dependencies).filter((key) =>
+				key.startsWith("nav-frontend-")
+			)
+		),
+		withCss,
+		withLess,
+		withTM,
+	],
 
+	{
+		i18n: {
+			locales: ["no"],
+			defaultLocale: "no",
+		},
 
-{
-  i18n: {
-    locales: ['no'],
-    defaultLocale: 'no',
-  },
+		basePath: "/sp",
 
-  async rewrites() {
-    // sett opp miljøvar
-    return process.env.NODE_ENV !== 'production'
-        ?
-            [
-                {
-                    source: '/oauth2/:path*',
-                    destination: `http://localhost:3005/oauth2/:path*`
-                },
-                {
-                    source: '/rest/:path*',
-                    destination: `http://localhost:3005/rest/:path*`
-                },
-            ]
-        :
-            [
-                {
-                    source: '/oauth2/:path*',
-                    destination: `https://digitalstatus.ekstern.dev.nav.no/oauth2/:path*`
-                },
-                {
-                    source: '/rest/:path*',
-                    destination: `https://digitalstatus.ekstern.dev.nav.no/rest/:path*`
-                }
-            ];
-    }
-});
+		async rewrites() {
+			// sett opp miljøvar
+			return process.env.NODE_ENV !== "production"
+				? [
+						{
+							source: "/oauth2/:path*",
+							destination: `http://localhost:3005/oauth2/:path*`,
+						},
+						{
+							source: "/rest/:path*",
+							destination: `http://localhost:3005/rest/:path*`,
+						},
+				  ]
+				: [
+						{
+							source: "/oauth2/:path*",
+							destination: `https://digitalstatus.ekstern.dev.nav.no/oauth2/:path*`,
+						},
+						{
+							source: "/rest/:path*",
+							destination: `https://digitalstatus.ekstern.dev.nav.no/rest/:path*`,
+						},
+				  ]
+		},
+	}
+)
