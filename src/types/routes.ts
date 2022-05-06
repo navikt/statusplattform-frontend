839c4c483c75dce810f7569f95f26dcb30138778
larsLotNav <lars.august.lotsberg@nav.no>
2022-05-06 14:09:09 +0200

    Test withouth rewrites

--- src/types/routes.ts
+++ src/types/routes.ts
@@ -139,7 +139,8 @@ export enum RouterLogin {
 
 export enum RouterOauthLogin {
     NAME = "LoginAzure",
-    PATH = "/oauth2/login?redirect="+ "/Dashboard/Privatperson"
+    //  PATH = "/authenticate/login"
+    PATH = "/oauth2/login?redirect="+ "sp/Dashboard/Privatperson"
 }
 
 export enum RouterLogout {
