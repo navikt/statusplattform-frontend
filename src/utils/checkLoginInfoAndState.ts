181a1e269f28f4851e4fd58245caec2e7817e895
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2022-05-10 13:14:01 +0200

    Test of cookie retrieval

--- src/utils/checkLoginInfoAndState.ts
+++ src/utils/checkLoginInfoAndState.ts
@@ -1,5 +1,6 @@
 import { UserData } from "../types/userData";
 import { EndPathGetLoginInfo } from "./apiHelper";
+import Cookies from 'cookies';
 
 
 export class ResponseError extends Error {
@@ -13,12 +14,11 @@ export const checkLoginInfoAndState = async (): Promise<UserData | null> => {
     let response;
     let endPath = EndPathGetLoginInfo();
 
-    response = await fetch(endPath, {
-        headers: {
-            "Authorization": "Bearer " + localStorage.getItem("Authorization")
+    response = await fetch(endPath);
 
-        }
-    });
+    const cookies = new Cookies(response, response)
+    
+    console.log(cookies)
 
     
     if (response.ok) {
