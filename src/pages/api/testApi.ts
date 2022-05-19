510e7c93458e4a295fa5e06cf1c3b105904ce1b8
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2022-05-19 13:57:46 +0200

    removed /rest/rest mistake

--- src/pages/api/testApi.ts
+++ src/pages/api/testApi.ts
@@ -4,40 +4,39 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const backendPath = process.env.BACKENDPATH
 
-const createRequest = (path,method ,headers)  => new Request(path, {
+const createRequest = (path,method, headers)  => new Request(path, {
     method: method,
     headers: headers
-  })
+})
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    console.log("IN TEST API");
+    console.log("IN TEST API")
+
     let headers = req.headers
     let backendEndpath = headers.backendendpath
     let method = req.headers.method
-    //const { ip } = req.cookies
+
     console.log(backendEndpath)
     console.log(method)
  
-    let path = backendPath + backendEndpath;
-
-    //let method = req.headers.method;
+    let path = backendPath + backendEndpath
     
-    let backEndRequest = createRequest(path, method, headers);
-
-    
+    let backEndRequest = createRequest(path, method, headers)
     
     let response = await fetch(backEndRequest)
-     await response.json().then(body => {
     
-        if (body) {
-            console.log("SUCCSESFULLY READ USERDATA")
-            res.status(200).json(body)
-        }
-        else {
-            res.send("Cant read userdate")
-        }
+    await response.json()
+        .then(body => {
+            if (body) {
+                console.log("SUCCSESFULLY READ USERDATA")
+                res.status(200).json(body)
+            }
+            else {
+                res.send("Cant read userdate")
+            }
 
-    }).catch(e => console.log(e))
+        })
+        .catch(e => console.log(e))
 
 
 };
