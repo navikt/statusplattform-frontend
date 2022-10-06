f7f1a1b048152e09f7408bfb686460b45f306cc7
larsLotNav <lars.august.lotsberg@nav.no>
2022-10-06 08:33:47 +0200

    Fjernet polling-url frontend

--- src/pages/Admin/NyTjeneste/index.tsx
+++ src/pages/Admin/NyTjeneste/index.tsx
@@ -169,26 +169,7 @@ const NewService = () => {
     }
 
     const validatePollingUrl = (urlInput) => {
-        if(urlInput.includes(" ")) {
-            return false
-        }
-        if(urlInput == "STATUSHOLDER") {
-            return true
-        }
-        if(urlInput.length == 0) {
-            return true
-        }
-
-        let url;
-        
-        try {
-            url = new URL(urlInput);
-        } catch (_) {
-            return false;  
-        }
-
-
-        return url.protocol === "http:" || url.protocol === "https:";
+        return true
     }
 
     const validateMonitorLink = (urlInput) => {
