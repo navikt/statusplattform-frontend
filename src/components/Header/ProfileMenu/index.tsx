f24cd258763bfb7cd695d7f56c3bc47ad07c76bc
Selma Hellum <selma.bergstad.hellum@nav.no>
2023-05-03 14:16:13 +0200

    fix: application error due to includes used on undefined list

--- src/components/Header/ProfileMenu/index.tsx
+++ src/components/Header/ProfileMenu/index.tsx
@@ -69,8 +69,14 @@ const ProfileMenu: React.FC<{
     const [anchorEl, setAnchorEl] = useState(undefined)
     const [anchor, setAnchor] = useState<HTMLSelectElement>(null)
 
-    const usersWithAccess = process.env.NEXT_PUBLIC_APPROVED_USERS?.split(",")
-    const usersWithOpsAccess = process.env.NEXT_PUBLIC_OPS_ACCESS?.split(",")
+    const usersWithAccess =
+        "L152423,H161540,K146221,J104568,G124938,M106261,G121973,H166137".split(
+            ","
+        )
+    const usersWithOpsAccess =
+        "L152423,K132081,H123099,L110875,K125327,F110862,A110886,L120166,G121973,H166137".split(
+            ","
+        )
 
     const handleSetOpen = (event) => {
         setOpen(!open)
