3f6db060dded6cab25c93fac8222feba62cca34c
Jonas Juvet <jonas.juvet@nav.no>
2025-09-25 14:05:49 +0200

    fix: perhaps this is the only faulty import?

--- src/pages/driftsmeldinger/index.tsx
+++ src/pages/driftsmeldinger/index.tsx
@@ -14,7 +14,7 @@ import { OpsMessageI } from "@/types/opsMessage"
 import { RouterError, RouterOpprettOpsMelding } from "@/types/routes"
 import { EndPathOps } from "@/utils/apiHelper"
 import { fetchOpsMessages } from "@/utils/opsAPI"
-import { HorizontalSeparator } from "../admin/index"
+import { HorizontalSeparator } from "@/components/Admin/AdminStyles"
 
 const CreateAvvikButtonWrapper = styled.div``
 const OpsHead = styled.div`
